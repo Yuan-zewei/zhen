c2da27bd8ff5c8c193b4fd276660d72f941bda30
远远泽伟 <599147860@qq.com>
2019-03-20 19:23:03 +0800

    查询

--- Training/views.py
+++ Training/views.py
@@ -12,30 +12,3 @@ def index(request):
 def post_list(request, id):
     post = Post.objects.get(id=id)
     return render(request, 'post_list.html', {'post': post})
-
-
-def post_create(request):
-    if request.method == 'POST':
-        post_form = PostForm(data=request.POST)
-        post_form.save()
-        return redirect('/')
-    else:
-        post_form = PostForm()
-    return render(request, 'post_create.html', {'post_form': post_form})
-
-
-def post_delete(request, id):
-    post = Post.objects.get(id=id)
-    post.delete()
-    return redirect('/')
-
-
-def post_update(request, id):
-    post = Post.objects.get(id=id)
-    if request.method == 'POST':
-        post_form = PostForm(data=request.POST, instance=post)
-        post_form.save()
-        return redirect('/')
-    else:
-        post_form = PostForm(instance=post)
-    return render(request, 'post_update.html', {'post_form': post_form})
