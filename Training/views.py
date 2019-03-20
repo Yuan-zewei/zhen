45d294dd723ed2a955415c2384c476a58da21156
远远泽伟 <599147860@qq.com>
2019-03-20 20:57:37 +0800

    修改查询方法

--- Training/views.py
+++ Training/views.py
@@ -14,21 +14,3 @@ def index(request):
 def post_list(request, id):
     post = Post.objects.get(id=id)
     return render(request, 'post_list.html', {'post': post})
-
-
-class PostUpdate(UpdateView):
-    model = Post
-    fields = ('title', 'publish', 'body')
-    template_name = 'post_update.html'
-    success_url = reverse_lazy('select')
-
-
-class PostCreate(CreateView):
-    model = Post
-    fields = ('title', 'body')
-    template_name = 'post_create.html'
-    success_url = reverse_lazy('index')
-
-    def form_valid(self, form):
-        form.instance.publish = timezone.now()
-        return super().form_valid(form)
