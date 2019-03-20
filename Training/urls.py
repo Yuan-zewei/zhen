f3af5a49aebc3da9b99ff382ca45a7bbbf4003e4
haozhen <971070495@qq.com>
2019-03-20 20:34:44 +0800

    添加文章

--- Training/urls.py
+++ Training/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/<int:id>/', views.post_list, name='post_list'),
-    path('update/<int:pk>/', views.PostUpdate.as_view(), name='update')
+    path('update/<int:pk>/', views.PostUpdate.as_view(), name='update'),
+    path('post/create/', views.PostCreate.as_view(), name='post_create'),
 ]
--- Training/views.py
+++ Training/views.py
@@ -3,7 +3,7 @@ from .models import Post
 from .forms import PostForm
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
@@ -21,3 +21,14 @@ class PostUpdate(UpdateView):
     fields = ('title', 'publish', 'body')
     template_name = 'post_update.html'
     success_url = reverse_lazy('select')
+
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ('title', 'body')
+    template_name = 'post_create.html'
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        form.instance.publish = timezone.now()
+        return super().form_valid(form)
